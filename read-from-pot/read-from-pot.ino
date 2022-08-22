6447459eea1bf369ed6ad5f6ec6bb2e111d51199
George Mauer <gmauer@gmail.com>
2022-08-22 17:21:56 -0500

    working out the potentiometer and dev process some more

--- read-from-pot/read-from-pot.ino
+++ read-from-pot/read-from-pot.ino
@@ -1,6 +1,18 @@
+int potentiometerValue = 500;
 
-void setup() {
+void setup()
+{
+  pinMode(A1, INPUT);
+  pinMode(9, OUTPUT);
+  Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
+  potentiometerValue = analogRead(A1);
+  digitalWrite(9, HIGH);
+  Serial.println(potentiometerValue);
+  delay(potentiometerValue); // Wait for potentiometerValue millisecond(s)
+  digitalWrite(9, LOW);
+  delay(potentiometerValue); // Wait for potentiometerValue millisecond(s)
 }
