723967e58f76bb6af98b8294e168b5f9f07480b9
George Mauer <gmauer@gmail.com>
2022-09-04 22:23:27 -0500

    internal timer stuff,

--- internal-timer/internal-timer.ino
+++ internal-timer/internal-timer.ino
@@ -4,14 +4,23 @@ struct LightState {
   bool isOn;
 };
 
-LightState state[2] {{ 1 }, { 0 }};
+struct SpeakerState {
+  int frequency;
+};
+
+struct State {
+  LightState lights[2];
+  SpeakerState speakers[1];
+};
+State state {{{ 1 }, { 0 }}, {{ 22 }} };
 
 void setup() {
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
 
   TCCR0A = (1 << WGM01); //CTC mode
-  OCR0A = 250; //This sets resulution to 1ms for some reason
+  OCR0A = 125; //This sets resulution to .5ms for some reason
 
   TIMSK0 |= (1 << OCIE0A); //set interrupt request
   sei(); //enable interrupt
@@ -23,14 +32,18 @@ void setup() {
 
 void loop() {
   // On and off every other third of a second
-  state[0] = { ((time / (1*333)) % 2) };
+  state.lights[0] = { ((time / (1*333)) % 2) };
 
   // On and off every 3/5ths second
-  state[1] = { ((time / (3*200)) % 2) };
+  state.lights[1] = { ((time / (3*200)) % 2) };
 
-  digitalWrite(4, state[0].isOn);
-  digitalWrite(3, state[1].isOn);
+  const int speakerState = { ((time / (2)) % (2000/state.speakers[0].frequency)) };
+
+  digitalWrite(4, state.lights[0].isOn);
+  digitalWrite(3, state.lights[1].isOn);
+  digitalWrite(5, speakerState);
 }
+
 
 ISR(TIMER0_COMPA_vect) { // interrupt request
   if(time == 0xFFFF) {
