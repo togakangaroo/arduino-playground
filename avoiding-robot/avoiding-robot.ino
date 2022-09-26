90672917368e13602e501d7c75c1d38c91d35be7
George Mauer <gmauer@gmail.com>
2022-09-25 22:57:24 -0500

    ino

--- avoiding-robot/avoiding-robot.ino
+++ avoiding-robot/avoiding-robot.ino
@@ -3,7 +3,7 @@ struct UltrasonicSensorPins {
   int echo;
 };
 
-const UltrasonicSensorPins ultrasonicSensors[1] = {{2, 3}};
+const UltrasonicSensorPins ultrasonicSensors[1] = {{10, 11}};
 
 struct MotorPins {
   int in1;
@@ -13,7 +13,9 @@ struct MotorPins {
 
 const MotorPins motors[2] = { {4, 5, 6}, {7, 8, 9}};
 
-const int speakerPins[1] { 10 };
+const int speakerPins[1] { 13 };
+
+int consequitiveTimesDistanceUnderThreshold = 0;
 
 void stopMotor(MotorPins motor) {
   digitalWrite(motor.in1, LOW);
@@ -61,18 +63,19 @@ void turnClockwise() {
 
 long readUltrasonicDistance(UltrasonicSensorPins ultrasonicSensor)
 {
-  pinMode(ultrasonicSensor.trigger, OUTPUT);  // Clear the trigger
   digitalWrite(ultrasonicSensor.trigger, LOW);
   delayMicroseconds(2);
   digitalWrite(ultrasonicSensor.trigger, HIGH);
   delayMicroseconds(10);
   digitalWrite(ultrasonicSensor.trigger, LOW);
-  pinMode(ultrasonicSensor.echo, INPUT);
   // Reads the echo pin, and returns the sound wave travel time in microseconds
-  return pulseIn(ultrasonicSensor.echo, HIGH);
+  long echoBack = pulseIn(ultrasonicSensor.echo, HIGH);
+  return echoBack * 0.01723;
 }
 
 void setup() {
+
+  Serial.begin(9600);
 
   for(MotorPins motor : motors) {
     pinMode(motor.ena, OUTPUT);
@@ -92,11 +95,35 @@ void setup() {
 }
 
 void loop() {
+  bool shouldBackOut = false;
   long closestDistance = 0xFFFF;
   for(UltrasonicSensorPins sensor : ultrasonicSensors) {
     long value = readUltrasonicDistance(sensor);
-    if(value < closestDistance) {
+    Serial.println(value);
+    if(0 < closestDistance && value < closestDistance) {
       closestDistance = value;
     }
    }
+  consequitiveTimesDistanceUnderThreshold += (0 < closestDistance && closestDistance < 30) ? 1 : 0;
+  
+  if(consequitiveTimesDistanceUnderThreshold >= 2) {
+    consequitiveTimesDistanceUnderThreshold = 0;
+    shouldBackOut = true;
+  }
+
+  if(shouldBackOut) {
+    for(int speaker : speakerPins) {
+      tone(speaker, 500);
+    }
+    goBackward();
+    delayMicroseconds(1000);
+    turnClockwise();
+    delayMicroseconds(800);
+    goBackward();
+  } else {
+    for(int speaker : speakerPins) {
+      noTone(speaker);
+    }
+    goForward();
+  }
 }
